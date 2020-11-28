8a60ff0c3d86bebd2d712d07b2a1b5552cf9fa0e
Dylan Nguyen <dylannguyen1000@gmail.com>
2020-11-28 14:07:28 -0500

    Added Logging

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from selenium.webdriver.common.action_chains import ActionChains
 # Mailjet
 from mailjet_rest import Client
 
+# Logging
+from logging import basicConfig, info, INFO
+
 # Constants / Secrets
 from config import config, secret
 
+# Basic Logging Config
+basicConfig(filename ='app.log', level = logging.INFO)
 
 # Set Firefox to Headless
 options = webdriver.FirefoxOptions()
@@ -16,28 +21,35 @@ options.headless=True
 
 # Create Driver Instance
 driver = webdriver.Firefox(options=options)
+info("Opening Firefox Headless")
 
 # Get Link
 driver.get(LINK)
+info("Opening Link")
 
 # Find Button for 1.16
 button = driver.find_element_by_name(BUTTON1_ELEMENT)
 user_input = driver.find_element_by_id(INPUT1_ELEMENT)
 ActionChains(driver).move_to_element(button).click(user_input).perform()
+info("Clicking 1.16 Button")
 
 # Find RSG Button
 button = driver.find_element_by_name(BUTTON2_ELEMENT)
 user_input = driver.find_element_by_id(INPUT2_ELEMENT)
 ActionChains(driver).move_to_element(button).click(user_input).perform()
+info("Clicking RSG Button")
 
 # Wait for Page Load
 driver.implicitly_wait(3)
 
 # Finds All Speedrunners
 content = driver.find_elements_by_css_selector(NAME_CSS_SELECTOR)
+info("Finding All Speedrunners")
+
 
 # Print Speedrunners
 for item in content:
+    info(f"Speedrunner: {item.text}")
     if (item.text) == USERNAME:
         message = "Elysaku Pog"
         mailjet = Client(auth=(MAILJET_API_KEY, MAILJET_API_SECRET), version='v3.1')
